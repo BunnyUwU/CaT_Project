3855a348e88cf03f9dfaa3e1025d6c209d8f85fd
BunnyUwU <caogiahungA2@gmail.com>
2019-11-20 16:35:28 +0700

    push

---
+++ Project/Project/Project/project.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+//#define QInt bitset<128>
+
+class QInt {
+private:
+	int arrayBit[4];
+public:
+	// Construtors
+	QInt();
+	QInt(const QInt&);
+
+	// Function
+	void add();
+	void multiply();
+
+	// operator +, -, *, /
+	QInt operator+(const QInt&);
+	QInt operator-(const QInt&);
+	QInt operator*(const QInt&);
+	QInt operator/(const QInt&);
+
+	// logic operator: AND, OR, XOR, NOT
+	QInt operator&(const QInt&);
+	QInt operator|(const QInt&);
+	QInt operator~();
+	QInt operator^(const QInt&);
+
+	// SHL, SHR
+	QInt operator>>(uint16_t);
+	QInt operator<<(uint16_t);
+};
